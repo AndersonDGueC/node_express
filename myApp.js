4d83f5bea16417606fa65b5e7ba3d2be20f6735c
anderson <adguerreroc@unal.edu.co>
2022-05-17 17:21:03 -0500

    style web

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 
 console.log('Hello World')
 
+app.use('/public', express.static(__dirname + '/public'));
+
 absolutePath = __dirname + '/views/index.html'
 app.get('/',function(req, res){
 	res.sendFile(absolutePath);
