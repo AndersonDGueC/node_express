729c1ee4fea183a8c51da1f36d720de075acc959
anderson <adguerreroc@unal.edu.co>
2022-05-17 17:36:48 -0500

    serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -1,16 +1,26 @@
 let express = require('express');
 let app = express();
 
+//console.log
 console.log('Hello World')
 
+//absolute path
+absolutePath = __dirname + '/views/index.html'
+
+//apply middleware before index
 app.use('/public', express.static(__dirname + '/public'));
 
-absolutePath = __dirname + '/views/index.html'
+//endpoint root
 app.get('/',function(req, res){
 	res.sendFile(absolutePath);
 	})
 
-
+//endpoint json
+app.get('/json', (req, res)=>{
+	res.json({
+		message:"Hello json"	
+	});
+})
 
 
 
